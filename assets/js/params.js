e16af668ff73cf964b30c49c52a86f02238a9d0b
Thiago Figueiredo <37218344+TN-Figueiredo@users.noreply.github.com>
2022-05-11 10:16:08 -0300

    feat: params

---
+++ assets/js/params.js
@@ -0,0 +1,24 @@
+// const params = new Proxy(new URLSearchParams(window.location.search), {
+//   get: (searchParams, prop) => searchParams.get(prop),
+// });
+
+// const src = params.src;
+
+const params = window.location.search;
+
+if (params) {
+// if (src) {
+  if (document.getElementById("cta-button-1")) {
+    document
+      .getElementById("cta-button-1")
+      .setAttribute("href", `https://pay.kiwify.com.br/MYLtYJJ${params}`);
+    //   .setAttribute("href", `https://pay.kiwify.com.br/MYLtYJJ?src=${src}`);
+  }
+
+  if (document.getElementById("cta-button-2")) {
+    document
+      .getElementById("cta-button-2")
+      .setAttribute("href", `https://pay.kiwify.com.br/MYLtYJJ${params}`);
+    //   .setAttribute("href", `https://pay.kiwify.com.br/MYLtYJJ?src=${src}`);
+  }
+}
