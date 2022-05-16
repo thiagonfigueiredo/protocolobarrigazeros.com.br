90d5c1ad59a5e22e84c46c7793a6a71c3e3718de
Thiago Figueiredo <37218344+TN-Figueiredo@users.noreply.github.com>
2022-05-16 13:15:06 -0300

    feat: go back kiwify

--- assets/js/params.js
+++ assets/js/params.js
@@ -4,10 +4,6 @@
 
 // const src = params.src;
 
-String.prototype.replaceAt = function(index, replacement) {
-  return this.substring(0, index) + replacement + this.substring(index + replacement.length);
-}
-
 const params = window.location.search;
 
 if (params) {
@@ -15,14 +11,14 @@ if (params) {
   if (document.getElementById("cta-button-1")) {
     document
       .getElementById("cta-button-1")
-      .setAttribute("href", `https://pay.hotmart.com/J70689920Y?checkoutMode=10&bid=1652713075701${params.replaceAt(0, '&')}`);
+      .setAttribute("href", `https://pay.kiwify.com.br/MYLtYJJ${params}`);
     //   .setAttribute("href", `https://pay.kiwify.com.br/MYLtYJJ?src=${src}`);
   }
 
   if (document.getElementById("cta-button-2")) {
     document
       .getElementById("cta-button-2")
-      .setAttribute("href", `https://pay.hotmart.com/J70689920Y?checkoutMode=10&bid=1652713075701${params.replaceAt(0, '&')}`);
+      .setAttribute("href", `https://pay.kiwify.com.br/MYLtYJJ${params}`);
     //   .setAttribute("href", `https://pay.kiwify.com.br/MYLtYJJ?src=${src}`);
   }
 }
