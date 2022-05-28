9154d18719798e4c70b691930da6aea44361fe1d
Thiago Figueiredo <37218344+TN-Figueiredo@users.noreply.github.com>
2022-05-28 13:43:24 -0300

    feat: update to kiwify

--- assets/js/params.js
+++ assets/js/params.js
@@ -6,19 +6,23 @@
 
 const params = window.location.search;
 
-String.prototype.replaceAt = function(index, replacement) {
-  return this.substring(0, index) + replacement + this.substring(index + replacement.length);
-}
+String.prototype.replaceAt = function (index, replacement) {
+  return (
+    this.substring(0, index) +
+    replacement +
+    this.substring(index + replacement.length)
+  );
+};
 
 if (params) {
-// if (src) {
+  // if (src) {
   if (document.getElementById("cta-button-1")) {
     document
       .getElementById("cta-button-1")
       // .setAttribute("href", `https://pay.hotmart.com/J70689920Y?checkoutMode=10&bid=1652713075701&src=${params.split("&")[1].split("=")[1] + "+" + params.split("&")[2].split("=")[1] + "+" + params.split("&")[3].split("=")[1]}`);
       // .setAttribute("href", `https://pay.hotmart.com/J70689920Y?checkoutMode=10&bid=1652713075701${params.replaceAt(0, "&")}`);
-    //   .setAttribute("href", `https://pay.kiwify.com.br/MYLtYJJ?src=${src}`);
-      .setAttribute("href", `https://ev.braip.com/checkout/pla2g26z/che5ezln${params}`);
+      .setAttribute("href", `https://pay.kiwify.com.br/MYLtYJJ${params}`);
+    // .setAttribute("href", `https://ev.braip.com/checkout/pla2g26z/che5ezln${params}`);
   }
 
   if (document.getElementById("cta-button-2")) {
@@ -26,7 +30,7 @@ if (params) {
       .getElementById("cta-button-2")
       // .setAttribute("href", `https://pay.hotmart.com/J70689920Y?checkoutMode=10&bid=1652713075701&src=${params.split("&")[1].split("=")[1] + "+" + params.split("&")[2].split("=")[1] + "+" + params.split("&")[3].split("=")[1]}`);
       // .setAttribute("href", `https://pay.hotmart.com/J70689920Y?checkoutMode=10&bid=1652713075701${params.replaceAt(0, "&")}`);
-    //   .setAttribute("href", `https://pay.kiwify.com.br/MYLtYJJ?src=${src}`);
-      .setAttribute("href", `https://ev.braip.com/checkout/pla2g26z/che5ezln${params}`);
+      .setAttribute("href", `https://pay.kiwify.com.br/MYLtYJJ${params}`);
+    // .setAttribute("href", `https://ev.braip.com/checkout/pla2g26z/che5ezln${params}`);
   }
 }
